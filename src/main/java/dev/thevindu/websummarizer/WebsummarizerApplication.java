df3bca4da5714633b87738f84fc6f6e4954068d4
Thevindu Wickramaarachchi <thevinduw@thev1ndu.local>
2025-10-25 10:00:38 +0530

    init

---
+++ src/main/java/dev/thevindu/websummarizer/WebsummarizerApplication.java
@@ -0,0 +1,13 @@
+package dev.thevindu.websummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsummarizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsummarizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/thevindu/websummarizer/WebsummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thevindu.websummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsummarizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
