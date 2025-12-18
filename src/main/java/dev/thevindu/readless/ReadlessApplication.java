b80d9d0114932e13e7354556a5829b2feebf6108
thev1ndu <itsthw9@gmail.com>
2025-12-18 18:00:26 +0530

    FIX: Naming issues

--- src/main/java/dev/thevindu/readless/ReadlessApplication.java
+++ src/main/java/dev/thevindu/readless/ReadlessApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebsummarizerApplication {
+public class ReadlessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebsummarizerApplication.class, args);
+        SpringApplication.run(ReadlessApplication.class, args);
     }
 
 }
